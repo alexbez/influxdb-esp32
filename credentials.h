1816e3b39a5d37658243d92136a27db8150e2da5
Alexander <alexbez@users.noreply.github.com>
2022-11-27 16:55:07 +0200

    Initial commit
    credentials.h template. Enter your own values

---
+++ credentials.h
@@ -0,0 +1,10 @@
+// Credentials
+// WiFi AP SSID
+#define WIFI_SSID ""
+// WiFi password
+#define WIFI_PASSWORD ""
+
+#define INFLUXDB_URL "http://hostname:8086"
+#define INFLUXDB_TOKEN ""
+#define INFLUXDB_ORG ""
+#define INFLUXDB_BUCKET ""
